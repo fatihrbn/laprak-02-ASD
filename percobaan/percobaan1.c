e7c8f2d9c9764d7e7cc74af23429aee75bd58bc0
M. Fatih Rabbani <fatihrbbni@gmail.com>
2022-03-14 19:00:18 +0700

    change tab width

--- percobaan/percobaan1.c
+++ percobaan/percobaan1.c
@@ -4,52 +4,52 @@
 typedef int ItemType;
 
 typedef struct {
-	int Item[MAXSTACK]; 
-	int Count;
+  int Item[MAXSTACK];
+  int Count;
 } Stack;
 
 void InitializeStack(Stack *S) {
-	S->Count = 0;
+  S->Count = 0;
 }
 
 int Empty(Stack *S) {
-	return (S->Count == 0);
+  return (S->Count == 0);
 }
 
 int Full(Stack *S) {
-	return (S->Count == MAXSTACK);
+  return (S->Count == MAXSTACK);
 }
 
 void Push(ItemType x, Stack *S) {
-	if (S->Count==MAXSTACK)
-		printf("Stack penuh! Data tidak dapat masuk!");
-	else {
-		S->Item[S->Count]=x;
-		++(S->Count);
-	}
+  if (S->Count==MAXSTACK)
+    printf("Stack penuh! Data tidak dapat masuk!");
+  else {
+    S->Item[S->Count]=x;
+    ++(S->Count);
+  }
 }
 
 int Pop(Stack *S) {
-	if (S->Count==0)
-		printf ("Stack masih kosong!");
-	else {
-		--(S->Count);
-		return (S->Item[S->Count]);
-	}
+  if (S->Count==0)
+    printf ("Stack masih kosong!");
+  else {
+    --(S->Count);
+    return (S->Item[S->Count]);
+  }
 }
 
 int main() {
-	int i,input;
-	Stack tumpukan;
-	InitializeStack(&tumpukan);
-	for (i=0; i<MAXSTACK; i++) {
-		printf ("Masukkan isi stack ke- %d : ",i+1);
-		scanf("%d",&input);
-		Push(input, &tumpukan);
-	}           
-	for (i=MAXSTACK; i>0; i--) {
-		printf("Isi stack ke- %d : ",i) ;
-		printf("%d \n",Pop(&tumpukan));
-	}
-	return 0;
+  int i,input;
+  Stack tumpukan;
+  InitializeStack(&tumpukan);
+  for (i=0; i<MAXSTACK; i++) {
+    printf ("Masukkan isi stack ke- %d : ",i+1);
+    scanf("%d",&input);
+    Push(input, &tumpukan);
+  }
+  for (i=MAXSTACK; i>0; i--) {
+    printf("Isi stack ke- %d : ",i) ;
+    printf("%d \n",Pop(&tumpukan));
+  }
+  return 0;
 }
